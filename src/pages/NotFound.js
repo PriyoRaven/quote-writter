4e2e17d6eb3b6e5d37a8022264cbdf45c29bdeeb
Raven <priyadarsi02@gmail.com>
2024-09-13 00:21:45 +0530

    Added and funtiionalised the comment part

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,7 +2,7 @@ const NotFound = () => {
   return (
     <div>
       <h2>404</h2>
-      <p>Page not found</p>
+      <p>Page not found.</p>
     </div>
   );
 };
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,12 +1,10 @@
-import { useParams } from "react-router-dom";
-import Comments from "../components/comments/Comments";
+import { useParams, Link, Outlet } from "react-router-dom";
 
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import { DUMMY_QUOTES } from "./AllQuotes";
 
 const QuoteDetail = () => {
   const params = useParams();
-
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
   if (!quote) {
@@ -16,7 +14,12 @@ const QuoteDetail = () => {
   return (
     <>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Comments />
+      <div className="centered">
+        <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+          Load Comments
+        </Link>
+      </div>
+      <Outlet />
     </>
   );
 };
